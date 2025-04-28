7e64c66559b7d8fc6e04212664ad08360685535e
Rodrigo Peixoto <107653217+ROpeixoto@users.noreply.github.com>
2025-04-28 21:03:11 +0000

    primeiras alterações

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req,res)=> {
+    res.send(message: 'Hello World!');
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,() =>{
+
+    console.log(`server is running on port http://localhost:${PORT}`)
+}
+
+)
