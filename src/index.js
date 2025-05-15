19bb06483bf49947a2cad4330cd84cb3b99f4781
Rodrigo Peixoto <107653217+ROpeixoto@users.noreply.github.com>
2025-05-15 04:05:17 +0000

    Adicionando CORS e alguns swaggers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import userRoute from './routes/user.route.js';
 import exampleRoute from './routes/example.route.js';
 import itemRoute from './routes/item.route.js';
 import User from './models/User.js';
+import swaggerRoute from './routes/swagger.route.js';
+import cors from 'cors';
 
 dotenv.config();
 db.connect();
@@ -13,8 +15,10 @@ await User.syncIndexes();
 
 const app = express();
 
+app.use(cors()); 
 app.use(express.json());
 
+app.use(swaggerRoute)
 app.use("/users", userRoute);
 app.use("/secureExampleRoute", exampleRoute);
 app.use("/items", itemRoute);
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,79 @@ import userController from '../controller/user.controller.js';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *     summary: Create a new user
+ *     description: Register a new user with username, email, and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: User's username.
+ *                 example: johndoe
+ *               email:
+ *                 type: string
+ *                 description: User's email address.
+ *                 example: user@example.com
+ *               password:
+ *                 type: string
+ *                 description: User's password.
+ *                 example: StrongPassword123
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *     responses:
+ *       201:
+ *         description: User successfully created.
+ *       400:
+ *         description: Bad request. Invalid input.
+ *       500:
+ *         description: Internal server error.
+ */
 router.post('/register', userController.register);
+
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: User login
+ *     description: Authenticate a user with email and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: User's email address.
+ *                 example: user@example.com
+ *               password:
+ *                 type: string
+ *                 description: User's password.
+ *                 example: StrongPassword123
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: User successfully authenticated.
+ *       400:
+ *         description: Bad request. Invalid input.
+ *       401:
+ *         description: Unauthorized. Invalid credentials.
+ *       500:
+ *         description: Internal server error.
+ */
 router.post('/login', userController.login);
 
 export default router;
